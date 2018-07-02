d32e41689104a657e64ab23cfc8cccbfbc26688c
rain-alex <rain_alex@mail.ru>
2018-07-02 17:51:39 +0300

    -added bottom space for social buttons -in case javascript is not available, static image of a map shown instead of google maps iframe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var searchForm = document.querySelector('.search-form');
 var openCloseButton = document.querySelector('.hotel-search-open-form-btn');
-openCloseButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    searchForm.classList.toggle('search-form-show');
+openCloseButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  searchForm.classList.toggle('search-form-show');
 });
+var htmlElement = document.querySelector('html');
+htmlElement.classList.remove('no-js');
+htmlElement.classList.add('js-on');
